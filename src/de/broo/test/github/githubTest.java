96ae56a95d6e38606b707dd1276881d928da748b
Rene Broo <r.broo@gmx.de>
2012-05-24 13:11:49 +0200

    inital commit

---
+++ src/de/broo/test/github/githubTest.java
@@ -0,0 +1,13 @@
+package de.broo.test.github;
+
+public class githubTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test erfolgreich");
+	}
+
+}
