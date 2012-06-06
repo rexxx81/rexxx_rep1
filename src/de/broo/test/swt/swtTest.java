b7a336fe9015a9588e5b886235bd73f517dce979
Rene Broo <r.broo@gmx.de>
2012-06-06 23:42:33 +0200

    06.06.12

--- src/de/broo/test/swt/swtTest.java
+++ src/de/broo/test/swt/swtTest.java
@@ -4,10 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-import org.apache.commons.net.ftp.FTP;
-import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.net.ftp.FTPFile;
-import org.apache.commons.net.ftp.FTPReply;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
@@ -25,13 +21,34 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.TabFolder;
 import org.eclipse.swt.widgets.TabItem;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.ProgressBar;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeColumn;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
 
 public class swtTest {
 	private static Text text;
 	static String selectedDir = null;
 	static String fileFilterPath = "C:\\Users\\Rexxx\\Dropbox\\Photos\\Export";
 	static String projectName = null;
+
 	private static Text text_1;
+	private static Text text_2;
+	private static Text text_3;
+	private static Text text_4;
+	private static Text text_5;
+
+	static int transferFtpAll = 0;
+	static int transferFtpLeft = 0;
+	private static Text text_6;
+	private static Text text_7;
+	private static Text text_8;
+	private static Text text_9;
+	private static Text text_10;
+	private static Text text_11;
+	private static Table table;
 
 	/**
 	 * @param args
@@ -39,12 +56,13 @@ public class swtTest {
 	public static void main(String[] args) {
 		Display display = new Display();
 		final Shell shell = new Shell(display);
-		shell.setMinimumSize(new Point(640, 480));
+		shell.setSize(494, 480);
+		shell.setMinimumSize(new Point(494, 480));
 		shell.setText("Rene's Ebay Manager v0.1");
 		shell.pack();
 
-		TabFolder tabFolder = new TabFolder(shell, SWT.NONE);
-		tabFolder.setBounds(0, 51, 632, 383);
+		final TabFolder tabFolder = new TabFolder(shell, SWT.NONE);
+		tabFolder.setBounds(0, 51, 486, 383);
 
 		TabItem tbtmArtikelnr = new TabItem(tabFolder, SWT.NONE);
 		tbtmArtikelnr.setText("Verzeichnis");
@@ -60,6 +78,78 @@ public class swtTest {
 		grpFtpUpload.setText("FTP- Upload");
 		grpFtpUpload.setBounds(10, 55, 459, 292);
 		grpFtpUpload.setVisible(false);
+
+		Group grpVon = new Group(grpFtpUpload, SWT.NONE);
+		grpVon.setText("Von");
+		grpVon.setBounds(10, 23, 439, 46);
+
+		text_2 = new Text(grpVon, SWT.BORDER);
+		text_2.setBounds(10, 17, 419, 19);
+
+		Group grpNach = new Group(grpFtpUpload, SWT.NONE);
+		grpNach.setText("Nach");
+		grpNach.setBounds(10, 75, 439, 46);
+
+		text_3 = new Text(grpNach, SWT.BORDER);
+		text_3.setBounds(10, 17, 419, 19);
+
+		final Group grpUploadProzess = new Group(grpFtpUpload, SWT.NONE);
+		grpUploadProzess.setText("Upload Prozess");
+		grpUploadProzess.setBounds(10, 202, 439, 80);
+		grpUploadProzess.setVisible(false);
+
+		Label lblNewLabel = new Label(grpUploadProzess, SWT.NONE);
+		lblNewLabel.setBounds(272, 25, 83, 13);
+		lblNewLabel.setText("Dateien Gesamt");
+
+		text_4 = new Text(grpUploadProzess, SWT.BORDER);
+		text_4.setBounds(380, 22, 49, 19);
+
+		Label lblDateienVerbleibend = new Label(grpUploadProzess, SWT.NONE);
+		lblDateienVerbleibend.setText("Dateien verbleibend");
+		lblDateienVerbleibend.setBounds(272, 47, 107, 13);
+
+		text_5 = new Text(grpUploadProzess, SWT.BORDER);
+		text_5.setBounds(380, 44, 49, 19);
+
+		final ProgressBar progressBar = new ProgressBar(grpUploadProzess,
+				SWT.NONE);
+		progressBar.setBounds(21, 42, 162, 18);
+
+		Label lblNewLabel_1 = new Label(grpUploadProzess, SWT.NONE);
+		lblNewLabel_1.setBounds(21, 25, 49, 13);
+		lblNewLabel_1.setText("Fortschritt");
+
+		Button btnNewButton_1 = new Button(grpFtpUpload, SWT.NONE);
+		btnNewButton_1.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+
+				if ((swtTestFtpUtil.checkFTP() == 1)) {
+					// System.out.println("hier gestoppt");
+					// System.out.println(String.valueOf(swtTestFtpUtil.checkFTP()));
+
+					MessageBox mb = new MessageBox(shell, SWT.OK);
+					mb.setText("Fehler");
+					mb.setMessage("Das Verzeichnis existiert bereits");
+					int val = mb.open();
+					text.setText("");
+					text_1.setText("");
+					grpFtpUpload.setVisible(false);
+
+				} else {
+					showUploadInfo(6, 6);
+					grpUploadProzess.setVisible(true);
+					swtTestFtpUtil.uploadToFtp();
+					progressBar.setSelection(100);
+					tabFolder.setSelection(1);
+
+				}
+			}
+
+		});
+		btnNewButton_1.setBounds(331, 127, 118, 23);
+		btnNewButton_1.setText("Upload starten");
 
 		Button btnNewButton = new Button(grpVerzeichnis, SWT.NONE);
 		btnNewButton.addListener(SWT.Selection, new Listener() {
@@ -104,6 +194,9 @@ public class swtTest {
 						projectName = getProjectName(selectedDir);
 						text_1.setText(projectName);
 						grpFtpUpload.setVisible(true);
+						text_2.setText(selectedDir);
+						text_3.setText(swtTestFtpUtil.getFtpHomeDir() + "/"
+								+ projectName);
 					}
 
 				}
@@ -114,11 +207,110 @@ public class swtTest {
 		btnNewButton.setBounds(401, 20, 68, 23);
 		btnNewButton.setText("Ausw\u00E4hlen");
 
-		TabItem tbtmFotos = new TabItem(tabFolder, SWT.NONE);
-		tbtmFotos.setText("Fotos");
+		TabItem tbtmKategorie = new TabItem(tabFolder, 0);
+		tbtmKategorie.setText("Kategorie");
+
+		Group grpKategorie = new Group(tabFolder, SWT.NONE);
+		grpKategorie.setText("Kategorie");
+		tbtmKategorie.setControl(grpKategorie);
+
+		text_11 = new Text(grpKategorie, SWT.BORDER);
+		text_11.setBounds(10, 22, 326, 19);
+
+		Group grpKategorieAuswhlen = new Group(grpKategorie, SWT.NONE);
+		grpKategorieAuswhlen.setText("Kategorie ausw\u00E4hlen");
+		grpKategorieAuswhlen.setBounds(10, 55, 459, 292);
+		
+		table = new Table(grpKategorieAuswhlen, SWT.BORDER | SWT.FULL_SELECTION);
+		table.setBounds(10, 20, 439, 238);
+		table.setHeaderVisible(true);
+		table.setLinesVisible(true);
+		
+		TableColumn tblclmnNr = new TableColumn(table, SWT.NONE);
+		tblclmnNr.setWidth(53);
+		tblclmnNr.setText("Nr.");
+		
+		TableColumn tblclmnName = new TableColumn(table, SWT.NONE);
+		tblclmnName.setWidth(178);
+		tblclmnName.setText("Name");
+		
+		Button btnAuswhlen = new Button(grpKategorieAuswhlen, SWT.NONE);
+		btnAuswhlen.setText("Ausw\u00E4hlen");
+		btnAuswhlen.setBounds(322, 264, 127, 23);
+
+		Button btnKategorieSuchen = new Button(grpKategorie, SWT.NONE);
+		btnKategorieSuchen.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			
+				
+				
+			}
+		});
+		btnKategorieSuchen.setText("Kategorie suchen");
+		btnKategorieSuchen.setBounds(342, 20, 127, 23);
 
 		TabItem tbtmBeschreibung = new TabItem(tabFolder, SWT.NONE);
 		tbtmBeschreibung.setText("Beschreibung");
+
+		Group grpBeschreibung = new Group(tabFolder, SWT.NONE);
+		grpBeschreibung.setText("Beschreibung");
+		tbtmBeschreibung.setControl(grpBeschreibung);
+
+		text_6 = new Text(grpBeschreibung, SWT.BORDER);
+		text_6.setBounds(10, 22, 385, 19);
+
+		Group group_1 = new Group(grpBeschreibung, SWT.NONE);
+		group_1.setText("FTP- Upload");
+		group_1.setBounds(10, 55, 459, 292);
+
+		Group group_2 = new Group(group_1, SWT.NONE);
+		group_2.setText("Von");
+		group_2.setBounds(10, 23, 439, 46);
+
+		text_7 = new Text(group_2, SWT.BORDER);
+		text_7.setBounds(10, 17, 419, 19);
+
+		Group group_3 = new Group(group_1, SWT.NONE);
+		group_3.setText("Nach");
+		group_3.setBounds(10, 75, 439, 46);
+
+		text_8 = new Text(group_3, SWT.BORDER);
+		text_8.setBounds(10, 17, 419, 19);
+
+		Group group_4 = new Group(group_1, SWT.NONE);
+		group_4.setVisible(false);
+		group_4.setText("Upload Prozess");
+		group_4.setBounds(10, 202, 439, 80);
+
+		Label label = new Label(group_4, SWT.NONE);
+		label.setText("Dateien Gesamt");
+		label.setBounds(272, 25, 83, 13);
+
+		text_9 = new Text(group_4, SWT.BORDER);
+		text_9.setBounds(380, 22, 49, 19);
+
+		Label label_1 = new Label(group_4, SWT.NONE);
+		label_1.setText("Dateien verbleibend");
+		label_1.setBounds(272, 47, 107, 13);
+
+		text_10 = new Text(group_4, SWT.BORDER);
+		text_10.setBounds(380, 44, 49, 19);
+
+		ProgressBar progressBar_1 = new ProgressBar(group_4, SWT.NONE);
+		progressBar_1.setBounds(21, 42, 162, 18);
+
+		Label label_2 = new Label(group_4, SWT.NONE);
+		label_2.setText("Fortschritt");
+		label_2.setBounds(21, 25, 49, 13);
+
+		Button button = new Button(group_1, SWT.NONE);
+		button.setText("Upload starten");
+		button.setBounds(331, 127, 118, 23);
+
+		Button button_1 = new Button(grpBeschreibung, SWT.NONE);
+		button_1.setText("Ausw\u00E4hlen");
+		button_1.setBounds(401, 20, 68, 23);
 
 		TabItem tbtmEintragen = new TabItem(tabFolder, SWT.NONE);
 		tbtmEintragen.setText("Eintragen");
@@ -155,13 +347,13 @@ public class swtTest {
 
 		Group grpProjektname = new Group(shell, SWT.NONE);
 		grpProjektname.setText("Projektname");
-		grpProjektname.setBounds(0, 0, 632, 45);
+		grpProjektname.setBounds(0, 0, 486, 45);
 
 		text_1 = new Text(grpProjektname, SWT.BORDER);
 		text_1.setEnabled(false);
-		text_1.setBounds(10, 16, 199, 19);
+		text_1.setBounds(10, 16, 231, 19);
 		shell.open();
-		checkFTP();
+		// checkFTP();
 
 		// Create and check the event loop
 		while (!shell.isDisposed()) {
@@ -171,67 +363,6 @@ public class swtTest {
 		}
 		display.dispose();
 
-	}
-
-	private static int checkFTP() {
-		int ftp_id = 0;
-		FTPClient ftp = new FTPClient();
-		String server = "www.kunsthaus-poorhosaini.de";
-		String username = "kunsthaus-6p";
-		String password = "Rhein@Kunst";
-		String directory = "httpdocs/ebay_rene";
-
-		try {
-			int reply;
-			ftp.connect(server);
-			ftp.login(username, password);
-			System.out.println("Connected to " + server + ".");
-			System.out.print(ftp.getReplyString());
-
-			// After connection attempt, you should check the reply code to
-			// verify
-			// success.
-			reply = ftp.getReplyCode();
-
-			if (!FTPReply.isPositiveCompletion(reply)) {
-				ftp.disconnect();
-				System.err.println("FTP server refused connection.");
-				System.exit(1);
-			}
-
-			// }
-
-			ftp.setFileType(FTP.BINARY_FILE_TYPE);
-			System.out.print(ftp.getReplyString());
-			ftp.enterLocalPassiveMode();
-			System.out.print("Enter local passive mode.\n");
-			ftp.changeWorkingDirectory(directory);
-			ftp.pwd();
-			System.out.print(ftp.getReplyString());
-
-			System.out.print(ftp.getReplyString());
-
-			FTPFile[] files = ftp.listDirectories("/" + directory + "/");
-			System.out.print(ftp.getReplyString());
-			for (FTPFile ftpFile : files) {
-				// System.out.println(ftpFile.getName());
-			}
-
-			ftp.logout();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (ftp.isConnected()) {
-				try {
-					ftp.disconnect();
-				} catch (IOException ioe) {
-					// do nothing
-				}
-			}
-			// System.exit(error ? 1 : 0);
-		}
-
-		return ftp_id;
 	}
 
 	public static int checkFiles(String dir) {
@@ -267,5 +398,37 @@ public class swtTest {
 		// System.out.println(token[token.length - 1]);
 		name = token[token.length - 1];
 		return name;
+	}
+
+	public static String getSelectedDir() {
+		return selectedDir;
+	}
+
+	public static void setSelectedDir(String selectedDir) {
+		swtTest.selectedDir = selectedDir;
+	}
+
+	public static String getFileFilterPath() {
+		return fileFilterPath;
+	}
+
+	public static void setFileFilterPath(String fileFilterPath) {
+		swtTest.fileFilterPath = fileFilterPath;
+	}
+
+	public static String getProjectName() {
+		return projectName;
+	}
+
+	public static void setProjectName(String projectName) {
+		swtTest.projectName = projectName;
+	}
+
+	public static void showUploadInfo(int all, int left) {
+
+		text_4.setText(String.valueOf(all));
+		text_4.pack();
+		text_5.setText(String.valueOf(left));
+		text_5.pack();
 	}
 }
---
+++ src/de/broo/test/swt/swtTestFtpUtil.java
@@ -0,0 +1,209 @@
+package de.broo.test.swt;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.apache.commons.net.ftp.FTPReply;
+
+public class swtTestFtpUtil {
+
+	static String server = "www.kunsthaus-poorhosaini.de";
+	static String username = "kunsthaus-6p";
+	static String password = "Rhein@Kunst";
+	static String ftpHomeDir = "httpdocs/ebay_rene";
+
+	public swtTestFtpUtil() {
+
+	}
+
+	public static void uploadToFtp() {
+		// TODO Auto-generated method stub
+		int ftp_id = 0;
+		FTPClient ftp = new FTPClient();
+
+		try {
+			int reply;
+			ftp.connect(server);
+			ftp.login(username, password);
+			System.out.println("Connected to " + server + ".");
+			System.out.print(ftp.getReplyString());
+
+			// After connection attempt, you should check the reply code to
+			// verify
+			// success.
+			reply = ftp.getReplyCode();
+
+			if (!FTPReply.isPositiveCompletion(reply)) {
+				ftp.disconnect();
+				System.err.println("FTP server refused connection.");
+				System.exit(1);
+			}
+
+			// }
+
+			ftp.setFileType(FTP.BINARY_FILE_TYPE);
+			// System.out.print(ftp.getReplyString());
+			ftp.enterLocalPassiveMode();
+			// System.out.print("Enter local passive mode.\n");
+			ftp.changeWorkingDirectory(ftpHomeDir);
+			ftp.pwd();
+			// System.out.print(ftp.getReplyString());
+			ftp.makeDirectory(swtTest.getProjectName());
+			// System.out.print(ftp.getReplyString());
+			ftp.changeWorkingDirectory(ftpHomeDir + "/"
+					+ swtTest.getProjectName());
+
+			File dir = new File(swtTest.selectedDir);
+			String localSourceFile = null;
+			String remoteResultFile = null;
+			File[] fileList = dir.listFiles();
+
+			int i = fileList.length;
+			
+			swtTest.showUploadInfo(fileList.length, i);
+			for (File f : fileList) {
+
+				remoteResultFile = ftpHomeDir + "/" + swtTest.getProjectName()
+						+ "/" + f.getName();
+
+				localSourceFile = swtTest.getSelectedDir() + "\\" + f.getName();
+
+				upload(localSourceFile, remoteResultFile, server, 21, username,
+						password, false);
+				i--; 
+				swtTest.showUploadInfo(fileList.length, i);
+
+				System.out.println("Upload -> " + f.getName());
+
+			}
+			System.out.println("Upload abgeschlossen");
+
+			ftp.logout();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (ftp.isConnected()) {
+				try {
+					ftp.disconnect();
+				} catch (IOException ioe) {
+					// do nothing
+				}
+			}
+			// System.exit(error ? 1 : 0);
+		}
+	}
+
+	public static int checkFTP() {
+		int ftpDirCheck = 0;
+		FTPClient ftp = new FTPClient();
+
+		try {
+			int reply;
+			ftp.connect(server);
+			ftp.login(username, password);
+
+			// System.out.println("Connected to " + server + ".");
+			// System.out.print(ftp.getReplyString());
+
+			// After connection attempt, you should check the reply code to
+			// verify
+			// success.
+
+			reply = ftp.getReplyCode();
+
+			if (!FTPReply.isPositiveCompletion(reply)) {
+				ftp.disconnect();
+				System.err.println("FTP server refused connection.");
+				System.exit(1);
+			}
+
+			FTPFile[] files = ftp.listDirectories("/" + ftpHomeDir + "/");
+			// System.out.print(ftp.getReplyString());
+			for (FTPFile ftpFile : files) {
+
+				if (ftpFile.getName().equals(swtTest.getProjectName())) {
+					ftpDirCheck = 1;
+				}
+
+			}
+
+			ftp.logout();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (ftp.isConnected()) {
+				try {
+					ftp.disconnect();
+				} catch (IOException ioe) {
+					// do nothing
+				}
+			}
+			// System.exit(error ? 1 : 0);
+		}
+
+		return ftpDirCheck;
+	}
+
+	public static boolean upload(String localSourceFile,
+			String remoteResultFile, String host, int port, String usr,
+			String pwd, boolean showMessages) throws IOException {
+		FTPClient ftpClient = new FTPClient();
+		FileInputStream fis = null;
+		boolean resultOk = true;
+
+		try {
+			ftpClient.connect(host, port);
+			if (showMessages) {
+				System.out.println(ftpClient.getReplyString());
+			}
+			resultOk &= ftpClient.login(usr, pwd);
+			if (showMessages) {
+				System.out.println(ftpClient.getReplyString());
+			}
+
+			resultOk &= ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+			if (showMessages) {
+				System.out.println(ftpClient.getReplyString());
+			}
+
+			// resultOk &= ftpClient.enterLocalPassiveMode();
+			// if (showMessages) {
+			// System.out.println(ftpClient.getReplyString());
+			// }
+
+			fis = new FileInputStream(localSourceFile);
+			resultOk &= ftpClient.storeFile(remoteResultFile, fis);
+			if (showMessages) {
+				System.out.println(ftpClient.getReplyString());
+			}
+			resultOk &= ftpClient.logout();
+			if (showMessages) {
+				System.out.println(ftpClient.getReplyString());
+			}
+		} finally {
+			try {
+				if (fis != null) {
+					fis.close();
+				}
+			} catch (IOException e) {/* nothing to do */
+			}
+			ftpClient.disconnect();
+		}
+
+		return resultOk;
+	}
+
+	public static String getFtpHomeDir() {
+		return ftpHomeDir;
+	}
+
+	public static void setFtpHomeDir(String ftpHomeDir) {
+		swtTestFtpUtil.ftpHomeDir = ftpHomeDir;
+	}
+
+}
